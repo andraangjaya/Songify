7dc1fe9f704c7d345c101ad45c70db49a51fe09d
andra <andrabaktiangjaya@gmail.com>
2025-10-30 04:50:32 +0800

    sign up fixes

--- js/home-page.js
+++ js/home-page.js
@@ -5,6 +5,42 @@ burgerBtn.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
+function searchSongs() {
+  const input = document.getElementById('searchInput').value.toLowerCase();
+  const cards = document.querySelectorAll('.card');
+  let found = false; // penanda apakah ada hasil yang cocok
+
+  cards.forEach(card => {
+    const title = card.querySelector('.card-title').innerText.toLowerCase();
+    const artist = card.querySelector('.card-desc').innerText.toLowerCase();
+    if (title.includes(input) || artist.includes(input)) {
+      card.style.display = '';
+      found = true;
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+const clickableCards = document.querySelectorAll('.card, .discover-more-cards, .universal-card-img-style');
+const audio = document.getElementById('audio-player');
+
+clickableCards.forEach(card => {
+  card.addEventListener('click', () => {
+    const src = card.dataset.audio;
+    if (audio.src.includes(src)) {
+      if (audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    } else {
+      audio.src = src;
+      audio.play();
+    }
+  });
+});
+
 function openPopup(id) {
   document.getElementById(id).style.display = "flex";
 }
@@ -41,41 +77,4 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
   closePopup("signupPopup");
   hideAuthButton();
   showSuccess("Sign Up Successful!");
-});
-
-
-function searchSongs() {
-  const input = document.getElementById('searchInput').value.toLowerCase();
-  const cards = document.querySelectorAll('.card');
-  let found = false; // penanda apakah ada hasil yang cocok
-
-  cards.forEach(card => {
-    const title = card.querySelector('.card-title').innerText.toLowerCase();
-    const artist = card.querySelector('.card-desc').innerText.toLowerCase();
-    if (title.includes(input) || artist.includes(input)) {
-      card.style.display = '';
-      found = true;
-    } else {
-      card.style.display = 'none';
-    }
-  });
-}
-
-const clickableCards = document.querySelectorAll('.card, .discover-more-cards, .universal-card-img-style');
-const audio = document.getElementById('audio-player');
-
-clickableCards.forEach(card => {
-  card.addEventListener('click', () => {
-    const src = card.dataset.audio;
-    if (audio.src.includes(src)) {
-      if (audio.paused) {
-        audio.play();
-      } else {
-        audio.pause();
-      }
-    } else {
-      audio.src = src;
-      audio.play();
-    }
-  });
 });
